9dc5211b524ab84abd671a96caf9eae7cec229ba
Медвєдев Глєб <129577660+gleb226@users.noreply.github.com>
2023-04-01 08:34:45 +0000

    added work, rest, money and time

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+import time
 class Person:
   age = 12
   height = 159.2
   isMail = True
+  money = 0
   name = 'Hlieb'
 
   def __init__(self, name, age, height):
@@ -9,15 +11,33 @@ class Person:
     self.age = age
     self.height = height
 
+  def work(self, days):
+    while days != 0:
+      time.sleep(5)
+      self.money += 15
+      days -= 1
+
+  def rest(self, days):
+    while days != 0:
+      time.sleep(5)
+      self.money -=10
+      days -=1
+
 me = Person('Hlieb', 12, 170)
 friend = Person('Anny', 30, 170)
 
-print(me.age)
-print(me.height)
-print(me.name)
-print(me.isMail)
-friend.name = 'Anny'
-print(friend.name)
-print(friend.name)
-print(friend.age)
-print(friend.height)
+print(me.money)
+me.work(5)
+print(me.money)
+me.rest(2)
+print(me.money)
+
+# print(me.age)
+# print(me.height)
+# print(me.name)
+# print(me.isMail)
+# friend.name = 'Anny'
+# print(friend.name)
+# print(friend.name)
+# print(friend.age)
+# print(friend.height)
